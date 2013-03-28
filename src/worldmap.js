429c8eb682d13f568874a5a88d083a58f33a80c0
Christian Benseler <cbenseler@gmail.com>
2013-03-28 12:43:02 -0300

    hover countries effect

--- src/worldmap.js
+++ src/worldmap.js
@@ -9,16 +9,33 @@ var WorldMap = function() {
 			},
 			fills : {
 				normal : "#AAAAAA",
-				hover : "#CC0000"
+				hover : "#DDDDDD"
 			}
 		}
+	}
+
+	var svg_borders = {};
+	var color_country = function(country) {
+		var i, l;
+		if (svg_borders.hasOwnProperty(country))
+			for (i=0, l= svg_borders[country].length;i<l;i++) {
+				svg_borders[country][i].animate({"fill":options.colors.fills.hover,"stroke":options.colors.borders.hover,"stroke-width":2},333);
+			}
+	}
+
+	var reset_country = function(country) {
+		var i, l;
+		if (svg_borders.hasOwnProperty(country))
+			for (i=0, l= svg_borders[country].length;i<l;i++) {
+				svg_borders[country][i].animate({"fill":options.colors.fills.normal,"stroke":options.colors.borders.normal,"stroke-width":1},333);
+			}
 	}
 
 	var get_paths = function() {
 		$
 		.when( $.getJSON("data/world_svg_paths_by_code.json") )
 		.done( function(data) {
-			var svg_borders = {};
+			
 			var paper = new Raphael(document.getElementById("canvas_container"), 1200, 600);
 			var border_color = "#000000";
 			var unselected_color = "#CC0000";
@@ -34,6 +51,14 @@ var WorldMap = function() {
 					//$(line.node).click( get_click_handler(country));
 					//$(line.node).mousemove( get_over_handler(country));
 					//$(line.node).mouseout( get_out_handler(country));
+
+					$(line.node)
+					.mousemove(function() {
+						color_country(country);
+					})
+					.mouseout(function() {
+						reset_country(country);
+					});
 
 					svg_borders[country].push(line);
 				}
