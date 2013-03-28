9e0fd3c56c1dc62ffb302e055aca259e62b2e0f4
Christian Benseler <cbenseler@gmail.com>
2013-03-28 16:58:06 -0300

    Country obj created + methods moved

--- src/worldmap.js
+++ src/worldmap.js
@@ -1,6 +1,8 @@
 
 var WorldMap = function() {
 
+	var countries = {};
+
 	var options = {
 		colors: {
 			borders : {
@@ -12,24 +14,7 @@ var WorldMap = function() {
 				hover : "#DDDDDD"
 			}
 		}
-	}
-
-	var svg_borders = {};
-	var color_country = function(country) {
-		var i, l;
-		if (svg_borders.hasOwnProperty(country))
-			for (i=0, l= svg_borders[country].length;i<l;i++) {
-				svg_borders[country][i].animate({"fill":options.colors.fills.hover,"stroke":options.colors.borders.hover,"stroke-width":2},333);
-			}
-	}
-
-	var reset_country = function(country) {
-		var i, l;
-		if (svg_borders.hasOwnProperty(country))
-			for (i=0, l= svg_borders[country].length;i<l;i++) {
-				svg_borders[country][i].animate({"fill":options.colors.fills.normal,"stroke":options.colors.borders.normal,"stroke-width":1},333);
-			}
-	}
+	};
 
 	var get_paths = function() {
 		$
@@ -40,35 +25,54 @@ var WorldMap = function() {
 			var border_color = "#000000";
 			var unselected_color = "#CC0000";
 			$.each(data, function(country, val) {
-				svg_borders[country]=[];
-				var line, i, path;
+				var country_obj = new Country(country);
+				var line, path;
 				for (var i=0, l=val.length;i<l;i++) {
-					line = paper.path(val[i]);
-					//line.attr({stroke:border_color,'stroke-width':1,'fill':unselected_color});
 
+					line = paper.path(val[i]);
 					line.attr({stroke:options.colors.borders.normal,'stroke-width':1,'fill':options.colors.fills.normal});
 					line.country=country;
-					//$(line.node).click( get_click_handler(country));
-					//$(line.node).mousemove( get_over_handler(country));
-					//$(line.node).mouseout( get_out_handler(country));
 
 					$(line.node)
-					.mousemove(function() {
-						color_country(country);
+					.mousemove(function(country_obj) {
+						countries[country].colorize(options);
 					})
 					.mouseout(function() {
-						reset_country(country);
+						countries[country].uncolorize(options);
 					});
 
-					svg_borders[country].push(line);
+					country_obj.borders.push(line);
 				}
+
+				countries[country] = country_obj;
 			});
-			//alert(svg_borders.toSource())
 		});
-	}
+	};
 
 	get_paths();
 
 	return this;
 
+};
+
+
+var Country = function(name) {
+	this.name = name;
+	this.borders = [];
+
+	this.colorize = function(options) {
+		var country = this;
+		for (var i=0, l = country.borders.length;i<l;i++) {
+			country.borders[i].animate({"fill":options.colors.fills.hover,"stroke":options.colors.borders.hover,"stroke-width":2},333);
+		}
+	};
+
+	this.uncolorize = function(options) {
+		var country = this;
+		for (var i=0, l = country.borders.length;i<l;i++) {
+			country.borders[i].animate({"fill":options.colors.fills.normal,"stroke":options.colors.borders.normal,"stroke-width":1},333);
+		}
+	};
+
+	return this;
 }
